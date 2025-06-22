a2ea728f6359b4a87d4b521642c6f171574513ca
sitdikov <SitdikovYUM-ext@vip.cbr.ru>
2025-06-22 13:03:30 +0700

    подключние к серверу

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ fi
 docker pull brawleryura1/tg_splitter_adapter-app:latest
 docker stop tg_splitter_adapter-app || true
 docker rm tg_splitter_adapter-app || true
-docker run -d --name tg_splitter_adapter-app --network my-network -p 8081:8081 -e BOT_TOKEN="$1" brawleryura1/tg_splitter_adapter-app:latest
+docker run -d --name tg_splitter_adapter-app --network my-network -p 8081:8081 -e BOT_TOKEN="$1" -e BACKEND_URL="$2" brawleryura1/tg_splitter_adapter-app:latest
