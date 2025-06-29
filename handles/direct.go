59f8800468197475413960dd8102aec9daa34e25
Igor Ignatenko <rireroofc@gmail.com>
2025-06-29 23:27:00 +0700

    fix text

--- handles/direct.go
+++ handles/direct.go
@@ -252,7 +252,7 @@ func HandleDirectMessages(update *tgbotapi.Update, bot *tgbotapi.BotAPI, api *cl
 			bot.Send(msg)
 		case "Присоединиться к группе":
 			userStates[userID] = "waiting_group_id_join"
-			msg := tgbotapi.NewMessage(chatID, "Напишите название вашей новой группы")
+			msg := tgbotapi.NewMessage(chatID, "Напишите id группы, к которой хотите присоединиться")
 			bot.Send(msg)
 		case "Создать группу":
 			userStates[userID] = "waiting_group_name_create_selection"
@@ -430,7 +430,7 @@ func HandleDirectMessages(update *tgbotapi.Update, bot *tgbotapi.BotAPI, api *cl
 
 			for i, expense := range result {
 				msg.Text += strconv.Itoa(i+1) + ".\n"
-				msg.Text += "Название группы: " + *expense.Title + "\n"
+				msg.Text += "Название траты: " + *expense.Title + "\n"
 				msg.Text += "Общая сумма: " + strconv.Itoa(int(*expense.TotalAmount)) + "\n"
 				msg.Text += "Id траты: `" + expense.Id.String() + "`\n\n"
 			}
@@ -874,14 +874,13 @@ func HandleDirectMessages(update *tgbotapi.Update, bot *tgbotapi.BotAPI, api *cl
 					)
 					msg = tgbotapi.NewMessage(chatID, "Подробная информация о трате: \n")
 					msg.Text += "Название траты: " + *result.Title + "\n"
-					msg.Text += "Id_траты: `" + result.Id.String() + "`\n"
 					total_amount := int(*result.TotalAmount)
 					msg.Text += "Сумма траты: " + strconv.Itoa(total_amount) + "\n"
 					msg.Text += "Пользователи: \n"
 
 					for i, user := range *result.Shares {
 						msg.Text += strconv.Itoa(i+1) + ": \n"
-						msg.Text += "Id человека: `" + user.UserId.String() + "`\n"
+						msg.Text += "Имя человека: `" + *user.UserName + "`\n"
 						msg.Text += "Сколько должен: " + strconv.Itoa(int(*user.Amount)) + "\n"
 					}
 					msg.Text += "\n" + "Выберите действие с тратами:"
