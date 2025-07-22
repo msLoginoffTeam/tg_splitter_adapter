49f9f23adeb1505674ffe84e31a37b8923799fb9
msLoginoff <146209985+msLoginoff@users.noreply.github.com>
2025-07-22 16:54:42 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ BACKEND_URL="http://telegram-splitter-app:8080"
 docker network inspect my-network >/dev/null 2>&1 || \
   docker network create my-network
 
-docker pull brawleryura1/tg_splitter_adapter-app:latest
+docker pull maxloginoff/tg_splitter_adapter-app:latest
 docker stop tg_splitter_adapter-app || true
 docker rm tg_splitter_adapter-app || true
 
@@ -21,4 +21,4 @@ docker run -d \
   -p 8081:8081 \
   -e BOT_TOKEN="$1" \
   -e BACKEND_URL="$BACKEND_URL" \
-  brawleryura1/tg_splitter_adapter-app:latest
+  maxloginoff/tg_splitter_adapter-app:latest
